0d556ee3f0af52cc4d653fd7bd7a950349b224c0
cheney2001 <35183820+cheney2001@users.noreply.github.com>
2020-10-24 23:17:37 +0800

    增加表处理方法（未完成）

--- ClassQuery.py
+++ ClassQuery.py
@@ -69,6 +69,81 @@ class ClassQuery:
         return course
 
     @staticmethod
+    def _format_week(course_week: str):
+        """
+        处理周次字串
+        example : 1-4，8-12，15-16周
+        :param course_week: 周次字串
+        :return: week list
+        """
+        if course_week == '' or course_week == ' ' or course_week == None:
+            return None
+        course_week = course_week.replace("周", '')
+        week_str_list = course_week.split('，')
+        week = []
+        for week_str in week_str_list:
+            if '-' not in week_str:
+                week.append(int(week_str))
+                continue
+            start, end = week_str.split('-')
+            week.extend([i for i in range(int(start), int(end) + 1)])
+        return week
+
+    @staticmethod
+    def _format_node(course_node: str):
+        """
+        处理节次字串
+        example :  1,2节
+        :param course_node: 节次字段
+        :return: node list
+        """
+        if course_node == '' or course_node == ' ' or course_node == None:
+            return None
+        course_node = course_node.replace('节', '')
+        start, end = course_node.split(',')
+        return [i for i in range(int(start), int(end) + 1)]
+
+    @staticmethod
+    def _format_course_rows(top_table_rows: list, bottom_table_rows: list = None):
+        """
+        处理课表tr 行元素
+        :param top_table_rows: 班级课表元素List
+        :param bottom_table_rows: 整周课表元素List
+        :return:
+        """
+        for row in top_table_rows:
+            ths = row.find_elements_by_xpath('/td')
+            course_dict = tree()
+            # 周次
+            course_week = ths[5].text
+            course_week = ClassQuery._format_week(course_week=course_week)
+            # 节次
+            course_node = ths[8].text
+            course_node = ClassQuery._format_node(course_node=course_node)
+            # 班级
+            classes = ths[1].text
+            course_dict[classes] = tree()
+            example = {
+                "type": ths[0].text,
+                "name": ths[2].text,
+                "teacher_main": ths[3].text,
+                "teacher_ass": ths[4].text,
+                "location": ths[6].text,
+                "day": ClassQuery.MAP_DAYS[ths[7].text],
+                "remarks": ths[9].text,
+            }
+            if example["type"] == "":
+                for week in course_week:
+                    for node in course_node:
+                        if "week{}".format(str(week)) not in course_dict[classes].keys():
+                            course_dict[classes]["week{}".format(str(week))] = []
+                        example["node"] = node
+                        course_dict[classes]["week{}".format(str(week))].append(example)
+            elif example["type"] == "整周":
+                pass
+
+
+    @staticmethod
     def _matchCourse(driver: WebDriver):
         """
         匹配并处理课程表
@@ -76,18 +151,6 @@ class ClassQuery:
         :return:
         """
         rows = driver.find_elements_by_xpath('//table[@id="gvSchedule"]/tr')[1:]
-        for row in rows :
-            ths = row.find_elements_by_xpath('/th')
-            course_type = ths[0].text
-            course_class = ths[1].text
-            course_name = ths[2].text
-            course_teacher1 = ths[3].text
-            course_teacher2 = ths[4].text
-            course_week = ths[5].text
-            course_location = ths[6].text
-            course_day = ClassQuery.MAP_DAYS[ths[7].text]
-            course_node = ths[8].text
-            course_remarks = ths[9].text
 
 
 if __name__ == "__main__":
